59e0bf072cbb8579e8d32d4a5c4cccd0d32c4f78
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-05-15 12:14:07 +0200

    nrrd saving feature added

--- HCR-preprocessing.py
+++ HCR-preprocessing.py
@@ -7,16 +7,12 @@ import os
 import numpy as np
 from aicspylibczi import CziFile
 import re
-
+import nrrd
 ########Splitting Channels####################
 
-c_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmch1'
-original_path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1/data/czidata/original/'
-
-if not os.path.exists(original_path):
-    print(f'Creating {original_path}')
-    os.makedirs(original_path, exist_ok=True)
-
+file_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmch1'
+ref_channel_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmch1\{ref_name}'
+sig_channel_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmch1\signal'
 ref_ch_num = int(input('Enter Reference Channel Number:'))
 
 
@@ -35,16 +31,24 @@ def get_channel_names(imagefilename):
     print('sig_ch1_name:', sig_ch1_name)
     print('sig_ch2_name:', sig_ch2_name)
     print('embryo_name:', embryo_name)
-
     return ref_ch_name, embryo_name, sig_ch2_name, sig_ch1_name, fish_num
 
 
-for file in os.listdir(c_path):
+def create_channel_folder(path):
+    if not os.path.exists(path):
+        print(f'Creating {path}')
+        os.makedirs(path, exist_ok=True)
+        return path
+
+
+for file in os.listdir(file_path):
     if file.endswith('.czi'):
         print(file)
         ref_name, exp_name, sig2_name, sig1_name, f_num = get_channel_names(file)
+        create_channel_folder(ref_channel_path)
+        create_channel_folder(sig_channel_path)
 
-        czi = CziFile(os.path.join(c_path, file))
+        czi = CziFile(os.path.join(file_path, file))
         max_channels = range(*czi.dims_shape()[0]['C'])
         max_slices = range(*czi.dims_shape()[0]['Z'])
         for ch_num in max_channels:
@@ -54,19 +58,14 @@ for file in os.listdir(c_path):
                 imgarray, shp = czi.read_image(B=0, S=0, C=ch_num, T=0, Z=z_plane)
                 image_list.append(np.squeeze(imgarray))
 
-                # split_image = split_czi_channels(imagefilename)
                 if ref_ch_num == ch_num:
                     ref_channel_image = np.stack(image_list).astype('uint8')
+                    nrrd.write(os.path.join(ref_channel_path, f"{exp_name}_{f_num}_{ref_name}.nrrd"), ref_channel_image, index_order='C')
                 else:
                     sig_channel_image = np.stack(image_list).astype('uint8')
+                    nrrd.write(os.path.join(sig_channel_path, f"{exp_name}_{f_num}_{sig1_name}.nrrd"),
+                               sig_channel_image, index_order='C')
     else:
         print("Not a .czi file. Please recheck File format!")
 ##################################################################################################
 
-# with tiff.TiffWriter(os.path.join(original_path, f'{name}_ref.tif')) as tifw:
-#     print(f'Adding slice {z_plane} to {name}_ref.tif')
-#     tifw.write(np.stack(image_list).astype('uint8'))
-
-# with tiff.TiffWriter(os.path.join(original_path, f'{name}_sig.tif')) as tifw:
-#     print(f'Adding slice {z_plane} {name}_sig.tif')
-#     tifw.write(np.stack(image_list).astype('uint8'))
