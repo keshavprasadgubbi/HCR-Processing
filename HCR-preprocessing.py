e9c5529625139aa975f90f9638a0a663cba71c2d
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-05-17 10:31:56 +0200

    Add files via upload

--- HCR-preprocessing.py
+++ HCR-preprocessing.py
@@ -1,7 +1,7 @@
 # *** Author: Keshava Prasad Gubbi***
 # Script for pre-processing Inbal's HCR Lines.
 # DONE: Read the filenames and split and determine names of sig1, sig2 and ref channels
-# TODO: Split the Czi imagefilename into respective channels and save it as nrrd with respective names as per channels.
+# DONE: Split the Czi imagefilename into respective channels and save it as nrrd with respective names as per channels.
 
 import os
 import numpy as np
@@ -11,7 +11,7 @@ import nrrd
 ########Splitting Channels####################
 
 file_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmch1'
-ref_channel_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmch1\{ref_name}'
+ref_channel_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmch1\reference'
 sig_channel_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmch1\signal'
 ref_ch_num = int(input('Enter Reference Channel Number:'))
 
@@ -41,6 +41,19 @@ def create_channel_folder(path):
         return path
 
 
+def split_czi_channels(czifile):
+    czi = CziFile(czifile)
+    max_channels = range(*czi.dims_shape()[0]['C'])
+    max_slices = range(*czi.dims_shape()[0]['Z'])
+    for ch_num in max_channels:
+        image_list = []
+        for z_plane in max_slices:
+            print('imagefilename:', file, ', ch_num:', ch_num, ', z_plane:', z_plane)
+            imgarray, shp = czi.read_image(B=0, S=0, C=ch_num, T=0, Z=z_plane)
+            image_list.append(np.squeeze(imgarray))
+            return ch_num, image_list
+
+
 for file in os.listdir(file_path):
     if file.endswith('.czi'):
         print(file)
@@ -48,23 +61,17 @@ for file in os.listdir(file_path):
         create_channel_folder(ref_channel_path)
         create_channel_folder(sig_channel_path)
 
-        czi = CziFile(os.path.join(file_path, file))
-        max_channels = range(*czi.dims_shape()[0]['C'])
-        max_slices = range(*czi.dims_shape()[0]['Z'])
-        for ch_num in max_channels:
-            image_list = []
-            for z_plane in max_slices:
-                print('imagefilename:', file, ', ch_num:', ch_num, ', z_plane:', z_plane)
-                imgarray, shp = czi.read_image(B=0, S=0, C=ch_num, T=0, Z=z_plane)
-                image_list.append(np.squeeze(imgarray))
+        channel_number, i_list = split_czi_channels(os.path.join(file_path, file))
 
-                if ref_ch_num == ch_num:
-                    ref_channel_image = np.stack(image_list).astype('uint8')
-                    nrrd.write(os.path.join(ref_channel_path, f"{exp_name}_{f_num}_{ref_name}.nrrd"), ref_channel_image, index_order='C')
-                else:
-                    sig_channel_image = np.stack(image_list).astype('uint8')
-                    nrrd.write(os.path.join(sig_channel_path, f"{exp_name}_{f_num}_{sig1_name}.nrrd"),
-                               sig_channel_image, index_order='C')
+        if ref_ch_num == channel_number:
+            ref_channel_image = np.stack(i_list).astype('uint8')
+            nrrd.write(os.path.join(ref_channel_path, f"{exp_name}_{f_num}_{ref_name}.nrrd"), ref_channel_image,
+                       index_order='C')
+        else:
+            sig_channel_image = np.stack(i_list).astype('uint8')
+            nrrd.write(os.path.join(sig_channel_path, f"{exp_name}_{f_num}_{sig1_name}.nrrd"),
+                       sig_channel_image, index_order='C')
+
     else:
         print("Not a .czi file. Please recheck File format!")
 ##################################################################################################
