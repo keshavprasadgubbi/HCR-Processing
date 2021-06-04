4d0a87df0bb669ff7f542d86c29d962d15910a5a
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-06-04 14:13:03 +0200

    Add files via upload

--- HCR-preprocessing.py
+++ HCR-preprocessing.py
@@ -15,64 +15,61 @@ ref_channel_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmc
 sig_channel_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmch1\signal'
 ref_ch_num = int(input('Enter Reference Channel Number:'))
 
-
-def get_channel_names(imagefilename):
-    name, ext = imagefilename.split('.')
-    if ref_ch_num == 0:
-        a, sig_ch1_name, ref_ch_name, fish_num = re.split('_ch\d_', name)
-    elif ref_ch_num == 1:
-        a, ref_ch_name, sig_ch1_name, fish_num = re.split("_ch\d_", name)
-    else:
-        ref_ch_name, a, sig_ch1_name, fish_num = re.split("_ch\d_", name)
-    embryo_name, sig_ch2_name = a.rsplit('_', 1)
-
-    print('fish_num:', fish_num)
-    print('ref_ch_name:', ref_ch_name)
-    print('sig_ch1_name:', sig_ch1_name)
-    print('sig_ch2_name:', sig_ch2_name)
-    print('embryo_name:', embryo_name)
-    return ref_ch_name, embryo_name, sig_ch2_name, sig_ch1_name, fish_num
-
-
-def create_channel_folder(path):
-    if not os.path.exists(path):
-        print(f'Creating {path}')
-        os.makedirs(path, exist_ok=True)
-        return path
-
-
-def split_czi_channels(czifile):
-    czi = CziFile(czifile)
-    max_channels = range(*czi.dims_shape()[0]['C'])
-    max_slices = range(*czi.dims_shape()[0]['Z'])
-    for ch_num in max_channels:
-        image_list = []
-        for z_plane in max_slices:
-            print('imagefilename:', file, ', ch_num:', ch_num, ', z_plane:', z_plane)
-            imgarray, shp = czi.read_image(B=0, S=0, C=ch_num, T=0, Z=z_plane)
-            image_list.append(np.squeeze(imgarray))
-            return ch_num, image_list
-
+ref_num = int(input('Enter Reference Channel Number: (please enter 0 if refrence channel in image is channel1!)'))
 
 for file in os.listdir(file_path):
     if file.endswith('.czi'):
-        print(file)
-        ref_name, exp_name, sig2_name, sig1_name, f_num = get_channel_names(file)
+        print('Image name: ', file)
         create_channel_folder(ref_channel_path)
         create_channel_folder(sig_channel_path)
+        embryo_name, sig_ch1_name, sig_ch2_name, ref_ch_name = re.split(r'_ch\d_', file)
+        c, ext = ref_ch_name.split('.', 1)
+        ref_ch_name, fish_num = c.split('_', 1)
 
-        channel_number, i_list = split_czi_channels(os.path.join(file_path, file))
+        print('fish_num:', fish_num)
+        print('ref_ch_name:', ref_ch_name)
+        print('sig_ch1_name:', sig_ch1_name)
+        print('sig_ch2_name:', sig_ch2_name)
+        print('embryo_name:', embryo_name)
 
-        if ref_ch_num == channel_number:
-            ref_channel_image = np.stack(i_list).astype('uint8')
-            nrrd.write(os.path.join(ref_channel_path, f"{exp_name}_{f_num}_{ref_name}.nrrd"), ref_channel_image,
-                       index_order='C')
-        else:
-            sig_channel_image = np.stack(i_list).astype('uint8')
-            nrrd.write(os.path.join(sig_channel_path, f"{exp_name}_{f_num}_{sig1_name}.nrrd"),
-                       sig_channel_image, index_order='C')
+        c = CziFile(os.path.join(file_path, file))
+        max_channels = range(*c.dims_shape()[0]['C'])
+        max_slices = range(*c.dims_shape()[0]['Z'])
 
-    else:
-        print("Not a .czi file. Please recheck File format!")
+        # for ch_num in max_channels:
+        for ch_num in range(len(max_channels)):
+            print('Processing ch_num:', ch_num, '...........')
+            ref_image_list = sig1_image_list = sig2_image_list = []
+            for z_plane in max_slices:
+                imgarray, shp = c.read_image(B=0, S=0, C=ch_num, T=0, Z=z_plane)
+                if ch_num == 0 and ch_num == ref_num:
+                    ref_image_list.append(np.squeeze(imgarray))
+                elif ch_num == 1:
+                    sig1_image_list.append(np.squeeze(imgarray))
+                elif ch_num == 2:
+                    sig2_image_list.append(np.squeeze(imgarray))
+                else:
+                    print('Exceeded Maximum number of channels')
+
+        RImage = np.stack(ref_image_list).astype('uint8')
+
+        S1Image = np.stack(sig1_image_list).astype('uint8')
+
+        S2Image = np.stack(sig1_image_list).astype('uint8')
+
+        ### Writing the individual Channels into nrrd format
+        print('Writing the individual Channels into nrrd format!')
+
+        print(f'Creating Reference Channel nrrd file with name : {embryo_name}_{fish_num}_{ref_ch_name}.nrrd ')
+        nrrd.write(os.path.join(ref_channel_path, f"{embryo_name}_{fish_num}_{ref_ch_name}.nrrd"), RImage,
+                   index_order='C')
+        print(f'Creating Signal1 Channel nrrd file with name : {embryo_name}_{fish_num}_{sig_ch1_name}.nrrd')
+        nrrd.write(os.path.join(sig_channel_path, f"{embryo_name}_{fish_num}_{sig_ch2_name}.nrrd"), S1Image,
+                   index_order='C')
+        print(f'Creating Signal1 Channel nrrd file with name : {embryo_name}_{fish_num}_{sig_ch1_name}.nrrd')
+        nrrd.write(os.path.join(sig_channel_path, f"{embryo_name}_{fish_num}_{sig_ch2_name}.nrrd"), S2Image,
+                   index_order='C')
+
+        print(f'###################### Completed processing {file} ###################### ')
 ##################################################################################################
 
