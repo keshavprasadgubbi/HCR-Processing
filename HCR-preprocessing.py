af2806ff5c326ea7f5d5be560e8a5270d99493d8
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-07-07 05:44:14 +0200

    Add files via upload

--- HCR-preprocessing.py
+++ HCR-preprocessing.py
@@ -8,23 +8,32 @@ import numpy as np
 from aicspylibczi import CziFile
 import re
 import nrrd
+
+
 ########Splitting Channels####################
 
-file_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmch1'
-ref_channel_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmch1\reference'
-sig_channel_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20201204_pmch_pmch1\signal'
-ref_ch_num = int(input('Enter Reference Channel Number:'))
+file_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20210302_rspo1_cckb'
+preprocessed_path = file_path + '/preprocessed/'
 
-ref_num = int(input('Enter Reference Channel Number: (please enter 0 if refrence channel in image is channel1!)'))
 
+def create_channel_folder(path):
+    if not os.path.exists(path):
+        print(f'Creating {path}')
+        os.makedirs(path, exist_ok=True)
+        return path
+########Splitting Channels####################
+# ref_num = int(input('Enter Reference Channel Number: (please enter 0 if refrence channel in image is channel1!)'))
+ref_num = 0
 for file in os.listdir(file_path):
     if file.endswith('.czi'):
         print('Image name: ', file)
-        create_channel_folder(ref_channel_path)
-        create_channel_folder(sig_channel_path)
-        embryo_name, sig_ch1_name, sig_ch2_name, ref_ch_name = re.split(r'_ch\d_', file)
+        create_channel_folder(preprocessed_path)
+
+        e_name, sig_ch1_name, sig_ch2_name, ref_ch_name = re.split(r'_ch\d_', file)
         c, ext = ref_ch_name.split('.', 1)
         ref_ch_name, fish_num = c.split('_', 1)
+        e1_name, f = e_name.rsplit('_', 1)
+        days, embryo_name = e1_name.split('_', 1)
 
         print('fish_num:', fish_num)
         print('ref_ch_name:', ref_ch_name)
@@ -42,6 +51,7 @@ for file in os.listdir(file_path):
             ref_image_list = sig1_image_list = sig2_image_list = []
             for z_plane in max_slices:
                 imgarray, shp = c.read_image(B=0, S=0, C=ch_num, T=0, Z=z_plane)
+
                 if ch_num == 0 and ch_num == ref_num:
                     ref_image_list.append(np.squeeze(imgarray))
                 elif ch_num == 1:
@@ -60,16 +70,14 @@ for file in os.listdir(file_path):
         ### Writing the individual Channels into nrrd format
         print('Writing the individual Channels into nrrd format!')
 
-        print(f'Creating Reference Channel nrrd file with name : {embryo_name}_{fish_num}_{ref_ch_name}.nrrd ')
-        nrrd.write(os.path.join(ref_channel_path, f"{embryo_name}_{fish_num}_{ref_ch_name}.nrrd"), RImage,
+        print(f'Creating Reference Channel nrrd file with name : {embryo_name}_{fish_num}_ch0_{ref_ch_name}.nrrd ')
+        nrrd.write(os.path.join(file_path, f"{embryo_name}_{fish_num}_ch0_{ref_ch_name}.nrrd"), RImage, index_order='C')
+        print(f'Creating nrrd file for Signal1 Channel with name : {embryo_name}_{fish_num}_ch1_{sig_ch1_name}.nrrd')
+        nrrd.write(os.path.join(file_path, f"{embryo_name}_{fish_num}_ch1_{sig_ch1_name}.nrrd"), S1Image,
                    index_order='C')
-        print(f'Creating Signal1 Channel nrrd file with name : {embryo_name}_{fish_num}_{sig_ch1_name}.nrrd')
-        nrrd.write(os.path.join(sig_channel_path, f"{embryo_name}_{fish_num}_{sig_ch2_name}.nrrd"), S1Image,
-                   index_order='C')
-        print(f'Creating Signal1 Channel nrrd file with name : {embryo_name}_{fish_num}_{sig_ch1_name}.nrrd')
-        nrrd.write(os.path.join(sig_channel_path, f"{embryo_name}_{fish_num}_{sig_ch2_name}.nrrd"), S2Image,
+        print(f'Creating nrrd file Signal2 Channel with name : {embryo_name}_{fish_num}_ch2_{sig_ch1_name}.nrrd')
+        nrrd.write(os.path.join(file_path, f"{embryo_name}_{fish_num}_ch2_{sig_ch2_name}.nrrd"), S2Image,
                    index_order='C')
 
         print(f'###################### Completed processing {file} ###################### ')
-##################################################################################################
 
