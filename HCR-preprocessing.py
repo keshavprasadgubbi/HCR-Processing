c08585df1d321e9a2695250b9f9890c7efeb9064
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-09-15 09:46:01 +0200

    Add files via upload

--- HCR-preprocessing.py
+++ HCR-preprocessing.py
@@ -14,9 +14,7 @@ import SimpleITK as sitk
 from numpy import ndarray
 
 file_path = r'C:\Users\keshavgubbi\Desktop\HCR\raw_data\20210302_rspo1_cckb'
-# preprocessed_path = file_path + '/preprocessed/'
-# ref_num = int(input('Enter Reference Channel Number: (please enter 0 if refrence channel in image is channel1!)'))
-ref_num = 0
+ref_num = 0  # the reference channel has been always set to 0 after consultation with Inbal.
 
 
 def create_channel_folder(path):
@@ -26,10 +24,10 @@ def create_channel_folder(path):
         return path
 
 
-def split_names(file):
-    e_name, signal_ch1_name, signal_ch2_name, reference_ch_name = re.split(r'_ch\d_', file)
-    c, ext = reference_ch_name.split('.', 1)
-    ref_ch_name, fish_number = c.split('_', 1)
+def split_names(f):
+    e_name, signal_ch1_name, signal_ch2_name, reference_ch_name = re.split(r'_ch\d_', f)
+    d, ext = reference_ch_name.split('.', 1)
+    ref_ch_name, fish_number = d.split('_', 1)
     e1_name, f = e_name.rsplit('_', 1)
     days, embryo_name = e1_name.split('_', 1)
     print('fish_num:', fish_number)
@@ -40,55 +38,49 @@ def split_names(file):
     return fish_number, ref_ch_name, signal_ch1_name, signal_ch2_name, embryo_name
 
 
-def read_voxel_size(f):
-    #The idea is to get the respective ndarray and convert it into a image and then obtain its voxel spacing and the
-    # use these values''
-    print("********Reading Voxel Size********")
-    im = sitk.GetImageFromArray(f)
-    width, height, depth = im.GetSpacing()
-    print('voxel spacing:', width, height)
-    return width, height
+def get_image_data(f):
+    num_stacks, h, w = f.shape[3:]
+    # Determine voxel spacing - x, y for use later while writing nrrd files to be of correct pixel spacing. This info
+    # can be verified by [ imageJ -> Properties]
+    voxel_x, voxel_y, voxel_z = f.get_physical_pixel_size()[:3]  # read_voxel_size(first_channel_data)
+    if voxel_z != 1e-6:
+        print("Unwanted Voxel depth Value. Will be reset to : 1e-6.")
+    return num_stacks, h, w, voxel_x, voxel_y, voxel_z
 
 
 ########Splitting Channels####################
 for file in os.listdir(file_path):
     if file.endswith('.czi'):
-        print('Image name: ', file)
+        print('Working with Image: ', file)
 
+        # Split the file name to obtain respective channel names and fish number for a given HCR line.
         fish_num, ref_ch_name, sig_ch1_name, sig_ch2_name, embryo_name = split_names(file)
+
+        # Creating folders for respective embryo/HCR line being analyzed
+        preprocessed_path = file_path + f'/preprocessed/{embryo_name}_{fish_num}/'
+        create_channel_folder(preprocessed_path)
 
         # Read the CZI Image. "c" will be the image object henceforth
         c = AICSImage(os.path.join(file_path, file))
-        *a, ch, dep, h, w = c.shape
-        print("Height, Width of image stack:", h, ",", w)
+        N_stacks, height, width, voxel_width, voxel_height, voxel_depth = get_image_data(c)
+        print("Height, Width of image stack:", height, ",", width, f"with {N_stacks} stacks!")
+        print("Voxel Details (x, y, depth):", voxel_width, ",", voxel_height, ",", voxel_depth)
 
         # Obtain the image data from respective channels
         first_channel_data: ndarray = c.get_image_data("ZYX", C=0, S=0, T=0)
         second_channel_data: ndarray = c.get_image_data("ZYX", C=1, S=0, T=0)
         third_channel_data: ndarray = c.get_image_data("ZYX", C=2, S=0, T=0)
 
-        # # Determine voxel spacing - x, y for use later while writing nrrd files
-        voxel_width, voxel_height = c.get_physical_pixel_size()[:2]#read_voxel_size(first_channel_data)
-        print(voxel_width, voxel_height)
-
-        # print(first_channel_data.shape)
+        # Stack the 3D ndarray to form an image stack, for each channel
         RImage: ndarray = np.stack(first_channel_data).astype('uint8')
         S1Image: ndarray = np.stack(second_channel_data).astype('uint8')
         S2Image: ndarray = np.stack(third_channel_data).astype('uint8')
 
-        # # Determine voxel spacing - x, y for use later while writing nrrd files
-        # voxel_width, voxel_height = read_voxel_size(RImage)
-        # # print(voxel_width, voxel_height)
-
-        processed_path = file_path + f'/processed/{embryo_name}_{fish_num}/'
-        create_channel_folder(processed_path)
-
         # # ### Writing the individual Channels into nrrd format
-        # print('Writing the individual Channels into nrrd format!')
-        #
-        # print(f'Creating Reference Channel nrrd file with name : {embryo_name}_{fish_num}_ch0_{ref_ch_name}.nrrd ')
-        nrrd.write(os.path.join(processed_path, f"{embryo_name}_{fish_num}_ch0_{ref_ch_name}.nrrd"), RImage,
-                   index_order='C', header={'units': ['m', 'm', 'm'], 'spacings': [voxel_width, voxel_height, 1e-6]})
+        print('Writing the individual Channels into nrrd format!')
+        print(f'Creating Reference Channel nrrd file with name : {embryo_name}_{fish_num}_ch0_{ref_ch_name}.nrrd ')
+        # nrrd.write(os.path.join(processed_path, f"{embryo_name}_{fish_num}_ch0_{ref_ch_name}.nrrd"), RImage,
+        #            index_order='C', header={'units': ['m', 'm', 'm'], 'spacings': [voxel_width, voxel_height, 1e-6]})
         # print(f'Creating nrrd file for Signal1 Channel with name : {embryo_name}_{fish_num}_ch1_{sig_ch1_name}.nrrd')
         # nrrd.write(os.path.join(processed_path, f"{embryo_name}_{fish_num}_ch1_{sig_ch1_name}.nrrd"), S1Image,
         #            index_order='C')
